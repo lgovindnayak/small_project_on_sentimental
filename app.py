416207d31de5e1528edcde0df5c80385be8a7e93
lgovindnayak <84020027+lgovindnayak@users.noreply.github.com>
2021-06-01 12:06:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+%%writefile app.py
 import streamlit as st
 import joblib
-import requests
-from bs4 import BeautifulSoup
+
 import pandas as pd
 import numpy as np
 import os
-import nltk 
+import nltk # nltk : natural process toolkit, used for remove the stop words
 import contractions
 import re
 from nltk.tokenize.toktok import ToktokTokenizer
@@ -16,48 +16,26 @@ stopword_list.remove('no')
 stopword_list.remove('not')
 tokenizer = ToktokTokenizer()
 
+df = joblib.load('sentiment.csv')
 
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-vs = SentimentIntensityAnalyzer()
 
-urls = ['https://inshorts.com/en/read/sports',
-        'https://inshorts.com/en/read/world',
-        'https://inshorts.com/en/read/politics']
-
-def build_dataset(urls):
-  news_data = []
-  for url in urls:
-    news_category = url.split('/')[-1]
-    data = requests.get(url)
-    soup = BeautifulSoup(data.content)
-    
-    news_articles = [{'news_headline':headline.find('span',attrs={"itemprop":"headline"}).string,
-                      'news_article':article.find('div',attrs={'itemprop':'articleBody'}).string,
-                      'news_category':news_category}
-                     
-                     for headline,article in zip(soup.find_all('div',class_=["news-card-title news-right-box"]),
-                                                 soup.find_all('div',class_=["news-card-content news-right-box"]))]
-    news_articles = news_articles[0:20]
-    news_data.extend(news_articles)
-
-  df = pd.DataFrame(news_data)   
-  df = df[["news_headline","news_article","news_category"]]
-  return df 
-
-df = build_dataset(urls)
-
+# 1. Lower case
 df.news_headline = df.news_headline.apply(lambda x:x.lower())
 df.news_article = df.news_article.apply(lambda x:x.lower())
 
+# 2. HTMP Tags
 df.news_headline = df.news_headline.apply(html_tag)
 df.news_article = df.news_article.apply(html_tag)
 
+# 3. Contractions
 df.news_headline = df.news_headline.apply(con)
 df.news_article = df.news_article.apply(con)
 
+# 4. Special Charcters
 df.news_headline = df.news_headline.apply(remove_sp)
 df.news_article = df.news_article.apply(remove_sp)
 
+# 5. Stop Words
 df.news_headline = df.news_headline.apply(remove_stopwords)
 df.news_article = df.news_article.apply(remove_stopwords)
 
